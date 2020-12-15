0dc52f5792a4b9c06d98740db93bf09594d1fc4e
shawn <shawnrobb@protonmail.com>
2020-12-15 16:29:21 +0000

    Constant model added

--- pledge/actions/admin.py
+++ pledge/actions/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from pledge.actions.models import Action
+from pledge.actions.models import Action, Constant
 
 
 @admin.register(Action)
 class ActionAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "description", "created", "modified")
+
+
+@admin.register(Constant)
+class ConstantAdmin(admin.ModelAdmin):
+    list_display = ("action", "identifier", "version", "value", "created", "modified")
+    list_filter = ("action",)
--- pledge/actions/models.py
+++ pledge/actions/models.py
@@ -6,3 +6,20 @@ from django_extensions.db.models import TitleSlugDescriptionModel, TimeStampedMo
 class Action(TimeStampedModel, TitleSlugDescriptionModel):
     def __str__(self):
         return self.title
+
+
+
+
+class Constant(TimeStampedModel):
+    action = models.ForeignKey(Action, on_delete=models.CASCADE)
+    version = models.PositiveSmallIntegerField(default=1)
+    identifier = models.CharField(
+        max_length=64
+    )  # Todo: add validator to constrain to specific characters (e.g. a-z and underscores)
+    value = models.FloatField()
+
+    class Meta:
+        unique_together = ("action", "version", "identifier")
+
+    def __str__(self):
+        return f"{self.action} ({self.identifier}) v{self.version}"
