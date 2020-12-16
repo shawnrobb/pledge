bb25e4e8d8e1e1d9c32c81916f02cbeb1a60d091
shawn <shawnrobb@protonmail.com>
2020-12-16 10:04:06 +0000

    Total pledges added to home page

--- pledge/home/views.py
+++ pledge/home/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 
 from django.views.generic import TemplateView
 
+from pledge.actions import helpers
+
 
 class HomeView(TemplateView):
     template_name = "pages/home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["total_pledges"] = helpers.get_total_pledges()
         return context
