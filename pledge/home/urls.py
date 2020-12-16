45028a411db905b104c1391b764f370eac2ffd61
shawn <shawnrobb@protonmail.com>
2020-12-16 15:10:19 +0000

    my pledges added to home / other users pledges added

--- pledge/home/urls.py
+++ pledge/home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomeView
+from .views import HomeView, UserPledgesView
 
 urlpatterns = [
+    path("pledges/<username>/", UserPledgesView.as_view(), name="user-pledges"),
     path("", HomeView.as_view(), name="home"),
 ]
--- pledge/home/views.py
+++ pledge/home/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
 
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
 
 from pledge.actions import helpers
 from pledge.actions.models import Metric, Pledge
+
+
+User = get_user_model()
 
 
 class HomeView(TemplateView):
@@ -19,4 +26,21 @@ class HomeView(TemplateView):
         for metric in Metric.objects.all():
             metrics.append((metric.title, helpers.get_total_impact(metric)))
         context["metrics"] = metrics
+        context["users"] = User.objects.exclude(id=self.request.user.id)
+        context["my_pledges"] = Pledge.objects.filter(user=self.request.user)
+        return context
+
+
+class UserPledgesView(DetailView):
+    """View to display the Pledges a user has made."""
+
+    template_name = "actions/user_pledges.html"
+    model = User
+
+    def get_object(self):
+        return get_object_or_404(self.model, username=self.kwargs["username"])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["pledges"] = Pledge.objects.filter(user=self.get_object())
         return context
